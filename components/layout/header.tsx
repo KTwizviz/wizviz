1c56fbab926e2204055f830f7b67deb2d82aa022
hayoung78 <wendy4332@naver.com>
2024-12-13 15:42:35 +0900

    ➕ WV-15 feat : header ui 생성

---
+++ components/layout/header.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import wizLogo from "@/assets/images/logo_ktwiz_en.svg";
+import ktSportsLogo from "@/assets/images/logo_ktsports.png";
+import Image from "next/image";
+import { MainMenuItems, MenuData } from "@/constants/header-menu";
+
+const Header = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  return (
+    <div className="relative">
+      <header className="w-full">
+        <nav
+          className={`transition-colors duration-200 text-SYSTEM-white ${
+            isHovering
+              ? "bg-SYSTEM-black text-SYSTEM-white border-b"
+              : "bg-black"
+          }`}
+        >
+          <div className="max-w-7xl mx-auto px-4">
+            <div className="flex items-center justify-between h-20">
+              {/* 로고 */}
+              <Link href="/">
+                <Image src={wizLogo} alt="wiz_logo" width={90} height={90} />
+              </Link>
+
+              {/* 중앙 메뉴 */}
+              <div
+                className="absolute left-1/2 transform -translate-x-1/2 flex items-center space-x-8"
+                onMouseEnter={() => setIsHovering(true)}
+                onMouseLeave={() => setIsHovering(false)}
+              >
+                {MainMenuItems.map((item) => (
+                  <Link key={item} href={`/${item.replace(" ", "-")}`}>
+                    {item}
+                  </Link>
+                ))}
+
+                {/* 드롭다운 패널 */}
+                {isHovering && (
+                  <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-screen bg-SYSTEM-black border-t shadow-lg">
+                    <div className="max-w-7xl mx-auto py-6">
+                      <div className="grid grid-cols-8 gap-4">
+                        {MainMenuItems.map((category) => (
+                          <div key={category} className="space-y-2">
+                            {MenuData[category]?.map((item) => (
+                              <Link
+                                key={item}
+                                href="#"
+                                className="block text-l hover:text-SYSTEM-main"
+                              >
+                                {item}
+                              </Link>
+                            ))}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              {/* 우측 메뉴 */}
+              <div className="flex items-center space-x-4 text-l">
+                <Link href="/login" className="hover:text-SYSTEM-main">
+                  로그인
+                </Link>
+                <span>|</span>
+                <Link href="/signup" className="hover:text-SYSTEM-main">
+                  회원가입
+                </Link>
+                <Link href="/sports" className="border border-ELSE-D9 p-1">
+                  <Image
+                    src={ktSportsLogo}
+                    alt="kt_sports_logo"
+                    width={50}
+                    height={50}
+                  />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;

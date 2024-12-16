dc7fdf6ab89a73b4329a235c956101ac1281c01d
재희 <hareppy329@gmail.com>
2024-12-16 19:51:28 +0900

    ♻️ [WV-18] refactor : 임의로 색상 적용한 부분 디자인 시스템 색상 적용

--- components/main/schedule.tsx
+++ components/main/schedule.tsx
@@ -11,10 +11,10 @@ const Schedule = () => {
       />
       <div className="w-[1100px] mx-auto -mt-8 relative">
         <div className="flex justify-between gap-5 px-10 py-[50px] mb-20 bg-SYSTEM-white rounded-[20px] shadow-[0_4px_20px_rgba(0,0,0,0.08)]">
-          <div className="w-[605px] h-[222px] bg-SYSTEM-white text-SYSTEM-black border border-gray-100 rounded-lg flex items-center justify-center">
+          <div className="w-[605px] h-[222px] bg-SYSTEM-white text-SYSTEM-black border border-l-ELSE-DE rounded-lg flex items-center justify-center">
             schedule
           </div>
-          <div className="w-[396px] h-[222px] bg-SYSTEM-white text-SYSTEM-black border border-gray-100 rounded-lg flex items-center justify-center">
+          <div className="w-[396px] h-[222px] bg-SYSTEM-white text-SYSTEM-black border border-ELSE-DE rounded-lg flex items-center justify-center">
             video
           </div>
         </div>

7c280af7e436c88bd8e89f4b10a78f1e9706e4e2
재희 <hareppy329@gmail.com>
2024-12-16 19:35:28 +0900

    ➕ [WV-18] feat : 메인페이지 배너, 스케줄 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div>main page</div>;
+import Image from "next/image";
+import MainPoster from "@/assets/images/2024_post_bg_web.png";
+import Schedule from "@/components/main/schedule";
+
+export default function Main() {
+  return (
+    <>
+      {/* main banner */}
+      <div className="relative">
+        <Image
+          src={MainPoster}
+          alt="main poster"
+          width={0}
+          height={0}
+          className="w-full mb-24"
+        />
+      </div>
+      {/* schedule */}
+      <Schedule />
+      {/* video */}
+      {/* gallery */}
+    </>
+  );
 }

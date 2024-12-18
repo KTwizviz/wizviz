3c102d678a3cf2b5be26a3c4360a35734f9dcdee
JinwooKim <rlawlsdn9583@gmail.com>
2024-12-18 16:59:05 +0900

    ➕ [WV-20] feat : Map 컴포넌트 생성

---
+++ components/wizpark/location/Map.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+
+const Map = ({ width, height }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_KAKAO_MAP_API}`;
+    script.async = true;
+
+    script.addEventListener("load", () => {
+      const kakao = window.kakao;
+
+      const mapContainer = document.getElementById("map");
+      const mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+
+      // 지도 생성
+      new kakao.maps.Map(mapContainer, mapOption);
+    });
+
+    document.head.appendChild(script);
+
+    // Clean Up
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div
+      id="map"
+      className={`w-${width} h-${height}`}
+    ></div>
+  );
+};
+
+export default Map;

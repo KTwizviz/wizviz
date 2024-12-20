4dd1a13f6d29f9efe89f3bd292e297d71c0120b8
JinwooKim <rlawlsdn9583@gmail.com>
2024-12-20 13:55:59 +0900

    ➕ WV-20 feat : 위경도 상수 데이터 분리리

--- components/wizpark/location/Map.tsx
+++ components/wizpark/location/Map.tsx
@@ -1,3 +1,4 @@
+import { LAT, LNG } from "@/constants/wizpark-location";
 import Script from "next/script";
 import React, { useEffect, useRef } from "react";
 
@@ -17,11 +18,12 @@ declare global {
 const KakaoMap = ({ size }: MapSize) => {
   const mapRef = useRef<HTMLDivElement>(null);
 
+  // 카카오맵 생성
   useEffect(() => {
     if (window.kakao && mapRef.current) {
       window.kakao.maps.load(() => {
         const mapOption = {
-          center: new window.kakao.maps.LatLng(37.2978428909635, 127.011348102567),
+          center: new window.kakao.maps.LatLng(LAT, LNG),
           level: 3,
           draggable: true,
           scrollwheel: true,
@@ -29,9 +31,25 @@ const KakaoMap = ({ size }: MapSize) => {
         };
 
         const map = new window.kakao.maps.Map(mapRef.current, mapOption);
+
+        //지도 마커 생성
+        const markerPosit = new window.kakao.maps.LatLng(LAT, LNG);
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosit
+        });
       });
     }
   }, []);
+
+  // 지도 마커 생성성
+  useEffect(() => {
+    const markerPosit = new window.kakao.maps.LatLng(33.450701, 126.570667);
+
+    const marker = new window.kakao.maps.Marker({
+      position: markerPosit
+    });
+  }, [])
+
 
   return (
     <div>

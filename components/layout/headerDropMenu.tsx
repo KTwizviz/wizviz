2742019872eb4ecdaf3acb3c412396ab108a9b34
hayoung78 <wendy4332@naver.com>
2024-12-22 23:32:30 +0900

    ♻️ WV-15 refactor : headerdrop 수정

--- components/layout/headerDropMenu.tsx
+++ components/layout/headerDropMenu.tsx
@@ -1,15 +1,14 @@
-import { MainMenuItems, MenuData } from "@/constants/header-menu";
+import { SubMenuItems } from "@/constants/header-menu";
 import Link from "next/link";
 
 const HeaderDropMenu = () => {
   return (
-    <div className="absolute mt-10 left-0 right-0  shadow-lg transition-all duration-300 ease-in-out overflow-hidden w-full animate-slideDown bg-SYSTEM-black items-center flex justify-center border-t border-t-ELSE-34">
+    <div className="invisible group-hover:visible absolute mt-10 left-0 right-0 shadow-lg transition-all duration-150 ease-in-out overflow-hidden w-full group-hover:animate-slideDown animate-slideUp bg-SYSTEM-black items-center flex justify-center border-t border-t-ELSE-34">
       <div className="grid grid-cols-7 w-[770px] py-4">
-        {MainMenuItems.map((category) => (
+        {Object.entries(SubMenuItems).map(([category, items]) => (
           <div key={category} className="flex flex-col items-center">
-            {/* 메뉴 아이템들 */}
             <div className="space-y-2">
-              {MenuData[category]?.map((item) => (
+              {items.map((item) => (
                 <Link
                   key={item}
                   href="#"

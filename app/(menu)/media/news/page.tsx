12f62e8fd2253bfc68e2bf87f897aa85e411b57d
hayoung78 <wendy4332@naver.com>
2024-12-27 14:40:45 +0900

    💄 WV-24 design : news-page ui 생성

--- app/(menu)/media/news/page.tsx
+++ app/(menu)/media/news/page.tsx
@@ -1,49 +1,25 @@
 import NewsCard from "@/components/media/news-card";
+import CustomPagination from "@/components/ui/custom-pagination";
+import { CustomSelectWithInput } from "@/components/ui/custom-select";
+import { NewsData } from "@/constants/news-data";
 
-export default function NewsPage() {
-  // 예시 데이터 배열
-  const newsData = [
-    {
-      id: 1,
-      title: "2024 kt wiz 73승 기념뱃지 수령",
-      content:
-        "안녕하세요 kt wiz입니다. 이번 시즌 73승 달성을 기념하여 특별 제작된 기념뱃지를 제작하였습니다...",
-      date: new Date("2024-01-23"),
-      views: 1234,
-    },
-    {
-      id: 2,
-      title: "2024 시즌 개막전 안내",
-      content: "2024 시즌 개막전 일정과 입장권 예매 안내드립니다...",
-      date: new Date("2024-01-22"),
-      views: 2345,
-    },
-    {
-      id: 3,
-      title: "구단 팬 사인회 개최",
-      content: "kt wiz 선수들과 함께하는 팬 사인회를 개최합니다...",
-      date: new Date("2024-01-21"),
-      views: 3456,
-    },
-    {
-      id: 4,
-      title: "구단 팬 사인회 개최",
-      content: "kt wiz 선수들과 함께하는 팬 사인회를 개최합니다...",
-      date: new Date("2024-01-21"),
-      views: 3456,
-    },
-    {
-      id: 5,
-      title: "구단 팬 사인회 개최",
-      content: "kt wiz 선수들과 함께하는 팬 사인회를 개최합니다...",
-      date: new Date("2024-01-21"),
-      views: 3456,
-    },
-  ];
+type NewsPageProps = {
+  searchParams: { page?: string };
+};
+export default function NewsPage({ searchParams }: NewsPageProps) {
+  const currentPage = Number(searchParams.page) || 1;
+  const itemsPerPage = 5;
+  const totalPages = Math.ceil(NewsData.length / itemsPerPage);
+
+  const currentItems = NewsData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
   return (
-    <div>
-      {newsData.map((news) => (
+    <>
+      <CustomSelectWithInput />
+      {currentItems.map((news) => (
         <NewsCard
           key={news.id}
           title={news.title}
@@ -52,6 +28,9 @@ export default function NewsPage() {
           views={news.views}
         />
       ))}
-    </div>
+      <div className="p-4">
+        <CustomPagination />
+      </div>
+    </>
   );
 }

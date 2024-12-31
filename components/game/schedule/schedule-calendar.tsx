43eb8cf19b76d3402e742ca63fbc8d24264dd69f
JinwooKim <rlawlsdn9583@gmail.com>
2024-12-31 19:00:19 +0900

    ✏️ WV-25 rename : 사용하지 않는 임포트 제거

--- components/game/schedule/schedule-calendar.tsx
+++ components/game/schedule/schedule-calendar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChevronLeft, ChevronRight, HomeIcon } from 'lucide-react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { useEffect, useState } from "react"
 import axios from 'axios';
 import Image from 'next/image';

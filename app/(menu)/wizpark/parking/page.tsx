ff395065830acd3a0f9e3b158267b867cc1671b5
naelumcat <naelumcat@gmail.com>
2025-01-03 13:02:01 +0900

    💄[WV-23] design : parking-page 컬러추가 및 코드 수정정

--- app/(menu)/wizpark/parking/page.tsx
+++ app/(menu)/wizpark/parking/page.tsx
@@ -14,16 +14,16 @@ export default function page() {
       <div className="pt-5 pb-8 pl-4">
         <p className="font-light">
           - 수원종합운동장 (수원 kt wiz park 포함) 주차장은 프로야구
-          <p className="pl-1 text-ELSE-EC0 font-medium inline">
+          <span className="pl-1 text-ELSE-EC0 font-medium inline">
             경기일에만 주차예약제
-          </p>
+          </span>
           가 시행됩니다.
         </p>
         <p className="font-light">
           -
-          <p className="pl-1 text-ELSE-EC0 font-medium inline">
+          <span className="pl-1 text-ELSE-EC0 font-medium inline">
             미예약 차량 주차 불가
-          </p>
+          </span>
           : 사전 예약차량만 주차 가능
         </p>
         <p className="text-ELSE-49 font-medium">
@@ -37,21 +37,21 @@ export default function page() {
           <p className="text-ELSE-EC0 text-xl font-medium">예약방법</p>
         </div>
         <div className="flex items-center gap-2">
-          <p className="flex items-center justify-center w-1 h-4 bg-red-400"></p>
+          <p className="w-1 h-4 bg-ELSE-F87"></p>
           <p className="text-lg">홈페이지</p>
         </div>
         <p className="pb-5 pt-2 pl-2 font-light">
           kt wiz, 수원종합운동장 홈페이지를 통한 예약/ 수정 및 취소 가능
         </p>
         <div className="flex items-center gap-2">
-          <p className="flex items-center justify-center w-1 h-4 bg-red-400"></p>
+          <p className="w-1 h-4 bg-ELSE-F87"></p>
           <p className="text-lg">예약 및 문의</p>
         </div>
         <p className="pb-5 pt-2 pl-2 font-light">
           수원종합운동장 031-240-2730~1
         </p>
         <div className="flex items-center gap-2">
-          <p className="flex items-center justify-center w-1 h-4 bg-red-400"></p>
+          <p className="w-1 h-4 bg-ELSE-F87"></p>
           <p className="text-lg">예약 기간</p>
         </div>
         <p className="pb-5 pt-2 pl-2 font-light">
@@ -59,7 +59,7 @@ export default function page() {
           만차시)
         </p>
         <div className="flex items-center gap-2">
-          <p className="flex items-center justify-center w-1 h-4 bg-red-400"></p>
+          <p className="w-1 h-4 bg-ELSE-F87"></p>
           <p className="text-lg">예약 제외 차량</p>
         </div>
         <p className="pb-5 pt-2 pl-2 font-light">
@@ -76,7 +76,7 @@ export default function page() {
           />
           <button className="absolute inset-0 flex justify-center items-center mt-20">
             <Link
-              className="bg-ELSE-EC0 text-white px-16 py-2 rounded-md"
+              className="bg-ELSE-EC0 text-SYSTEM-white px-16 py-2 rounded-md"
               href="https://suwonparkingbaseball.or.kr/suwonwps/EgovPageLink.do?link=main/menu/prs/ParkingReservationPrivacy"
               target="_blank"
               rel="noopener noreferrer"

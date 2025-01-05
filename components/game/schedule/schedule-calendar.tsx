861ee80e408993e5957c0d83619eddd940b5cbce
Ray <rlawlsdn9583@gmail.com>
2025-01-05 19:03:04 +0900

    ➕ WV-25 feat : 모든 스케줄 캘린더 모드 구현

--- components/game/schedule/schedule-calendar.tsx
+++ components/game/schedule/schedule-calendar.tsx
@@ -1,14 +1,18 @@
 'use client'
 
+import { Switch } from '@/components/ui/switch';
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import Image from 'next/image';
+import { useState } from 'react';
 
-const ScheduleCalendar = ({ date, schedules, monthHandler }: ScheduleCalendarProps) => {
+const ScheduleCalendar = ({ date, schedules, allSchedules, monthHandler }: ScheduleCalendarProps) => {
   const daysInMonth = new Date(date.year, date.month, 0).getDate()        // 현재 선택된 月의 마지막 날짜   
   const firstDayOfMonth = new Date(date.year, date.month - 1, 1).getDay() // 현재 선택된 月의 시작 요일
 
   const days = Array.from({ length: daysInMonth }, (_, i) => i + 1)
   const weekdays = ['일', '월', '화', '수', '목', '금', '토']
+
+  const [isAllScheduleMode, setIsAllScheduleMode] = useState(false);
 
   return (
     <div className="w-full mx-auto my-10">
@@ -24,6 +28,15 @@ const ScheduleCalendar = ({ date, schedules, monthHandler }: ScheduleCalendarPro
           <ChevronRight className="w-8 h-8" />
         </button>
       </div>
+      <div className='flex justify-end gap-2 pr-4'>
+        <Switch
+          checked={isAllScheduleMode}
+          onCheckedChange={(checked) => setIsAllScheduleMode(checked)}
+        />
+        <span className='font-medium text-sm text-ELSE-49'>
+          모든 경기
+        </span>
+      </div>
 
       <div className="grid grid-cols-7">
         {weekdays.map((day) => (
@@ -38,7 +51,9 @@ const ScheduleCalendar = ({ date, schedules, monthHandler }: ScheduleCalendarPro
 
         {days.map((day) => {
           const keyDate = `${date.year}${String(date.month).padStart(2, '0')}` + String(day).padStart(2, '0');
-          const todaySchedule = schedules.find((schedule) => schedule.displayDate === keyDate);
+          const ktSchedule = schedules.find((schedule) => schedule.displayDate === keyDate);
+          const allSchedule = allSchedules.filter((schedule) => schedule.displayDate === keyDate);
+          console.log(allSchedule)
 
           return (
             <div
@@ -47,48 +62,69 @@ const ScheduleCalendar = ({ date, schedules, monthHandler }: ScheduleCalendarPro
               ${day === date.today && date.year === date.year && date.month === date.month
                   ? 'border-SYSTEM-main'
                   : 'border-gray-100'}
-                ${todaySchedule && 'bg-ELSE-FF5'}` // 스케줄이 있는 날 배경 색상 적용
+                ${ktSchedule && 'bg-ELSE-FF5'}` // 스케줄이 있는 날 배경 색상 적용
               }
             >
               <div className="flex justify-between items-center font-medium mb-1">
-                <span className="flex-1">
-                  {day}
-                </span>
-                {todaySchedule?.outcome === '승' ?
-                  <div className='flex items-center text-white bg-SYSTEM-main rounded-xl px-1.5'>승</div>
-                  : todaySchedule?.outcome === '패' &&
-                  <div className='flex items-center text-white bg-ELSE-D9 rounded-xl px-1.5'>패</div>
-                }
+                <span className="flex-1">{day}</span>
+                {!isAllScheduleMode && ktSchedule?.outcome && (
+                  <div
+                    className={`flex items-center text-white rounded-xl px-1.5 ${ktSchedule.outcome === '승' ? 'bg-SYSTEM-main' : 'bg-ELSE-D9'
+                      }`}
+                  >
+                    {ktSchedule.outcome}
+                  </div>
+                )}
                 <div className="flex-1"></div>
               </div>
-
               {
-                todaySchedule && (
-                  <div className="w-full flex flex-col justify-center items-center text-m">
-                    <div className="flex items-center gap-1 mb-1">
-                      <Image
-                        src={todaySchedule.visitLogo}
-                        alt={todaySchedule.visit}
-                        width={48}
-                        height={48}
-                      />
-                      <span className='text-l'>vs</span>
-                      <Image
-                        src={todaySchedule.homeLogo}
-                        alt={todaySchedule.home}
-                        width={48}
-                        height={48}
-                      />
-                    </div>
-                    <p>{todaySchedule.gtime}</p>
-                    {todaySchedule.stadium === '수원' ?
-                      <p className='text-SYSTEM-main font-bold'>
-                        {todaySchedule.stadium}
-                      </p>
-                      : <p> {todaySchedule.stadium}</p>
-                    }
-                  </div>
-                )
+                isAllScheduleMode ?
+                  (allSchedule &&
+                    allSchedule.map((schedule) => {
+                      return (
+                        <div key={schedule.key} className='flex justify-center'>
+                          <Image
+                            src={schedule.visitLogo}
+                            alt={schedule.visit}
+                            width={24}
+                            height={24}
+                          />
+                          {schedule.home} vs {schedule.visit}
+                          <Image
+                            src={schedule.homeLogo}
+                            alt={schedule.home}
+                            width={24}
+                            height={24}
+                          />
+                        </div>
+                      )
+                    })
+                  ) :
+                  (ktSchedule &&
+                    (<div className="w-full flex flex-col justify-center items-center text-m">
+                      <div className="flex items-center gap-1 mb-1">
+                        <Image
+                          src={ktSchedule.visitLogo}
+                          alt={ktSchedule.visit}
+                          width={48}
+                          height={48}
+                        />
+                        <span className='text-l'>vs</span>
+                        <Image
+                          src={ktSchedule.homeLogo}
+                          alt={ktSchedule.home}
+                          width={48}
+                          height={48}
+                        />
+                      </div>
+                      <p>{ktSchedule.gtime}</p>
+                      {ktSchedule.stadium === '수원' ?
+                        <p className='text-SYSTEM-main font-bold'>
+                          {ktSchedule.stadium}
+                        </p>
+                        : <p> {ktSchedule.stadium}</p>
+                      }
+                    </div>))
               }
             </div>
           )

c22059ad5fd69dc5a158fe6b835be1945764d45e
hayoung78 <wendy4332@naver.com>
2025-01-06 13:40:44 +0900

    🔥 WV-35 remove : 불필요한 코드 삭제

--- components/player/coach-list.tsx
+++ components/player/coach-list.tsx
@@ -6,18 +6,16 @@ export async function CoachList() {
   console.log(coachList, "coachlist");
 
   return (
-    <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {coachList.map((coach) => (
-          <CoachCard
-            key={coach.backnum}
-            image={coach.mobilePlayerImg1}
-            name={coach.playerName}
-            backnum={parseInt(coach.backnum)}
-          />
-        ))}
-      </div>
-    </>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {coachList.map((coach) => (
+        <CoachCard
+          key={coach.backnum}
+          image={coach.mobilePlayerImg1}
+          name={coach.playerName}
+          backnum={parseInt(coach.backnum)}
+        />
+      ))}
+    </div>
   );
 }
 

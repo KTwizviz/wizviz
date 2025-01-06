8efb6b8d28c79078e251d36cc147420ecafe360a
hayoung78 <wendy4332@naver.com>
2025-01-06 15:12:09 +0900

    ♻️ WV-35 refactor: .env파일 수정 및 BASE_URL수정

--- api/player/apis.ts
+++ api/player/apis.ts
@@ -1,11 +1,11 @@
 import { CoachResponse } from "./types";
 
 // api/player/coachlist
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
 console.log(BASE_URL, "나 베이스유알엘");
 
 export const getCoachList = async (): Promise<CoachInfo[]> => {
-  const res = await fetch("http://54.180.228.165/api/player/coachlist", {
+  const res = await fetch(`${BASE_URL}/player/coachlist`, {
     method: "GET",
   });
 

0ce392016134faad440e6f736ff2034a9cbbe07f
hayoung78 <wendy4332@naver.com>
2025-01-06 15:59:07 +0900

    ♻️ WV-35 refactor : 환경변수 수정

--- api/player/apis.ts
+++ api/player/apis.ts
@@ -4,7 +4,6 @@ import { CoachResponse } from "./types";
 
 export const getCoachList = async (): Promise<CoachInfo[]> => {
   const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
-  console.log(BASE_URL, "나 베이스유알엘");
   const res = await fetch(`${BASE_URL}/player/coachlist`, { method: "GET" });
 
   if (!res.ok) {

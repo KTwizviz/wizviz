d5abbe6201d1d11f540831db770cea5922f196ad
hayoung78 <wendy4332@naver.com>
2025-01-06 13:25:52 +0900

    ➕ WV-35 feat : getCoachlist api 함수 생성

---
+++ api/player/apis.ts
@@ -0,0 +1,17 @@
+import { CoachResponse } from "./types";
+
+// api/player/coachlist
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const getCoachList = async (): Promise<CoachInfo[]> => {
+  const res = await fetch(`${BASE_URL}/player/coachlist`, {
+    method: "GET",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch coach list");
+  }
+
+  const response: CoachResponse = await res.json();
+  return response.data.list; // list 반환
+};

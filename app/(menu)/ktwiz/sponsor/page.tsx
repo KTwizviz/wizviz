520e34352a544da016a947cb9bd8cffc1a271fb0
naelumcat <naelumcat@gmail.com>
2025-01-09 14:05:01 +0900

    ♻️[WV-39] design : create-sponsor-page {} 제거

--- app/(menu)/ktwiz/sponsor/page.tsx
+++ app/(menu)/ktwiz/sponsor/page.tsx
@@ -6,10 +6,10 @@ export default function page() {
     <div className="pt-4">
       <div className="flex justify-end mb-8">
         <Link
-          className={"bg-ELSE-EC0 py-2 px-4 text-SYSTEM-white rounded-md"}
-          href={"https://b2b.ktwiz.co.kr/about/intro"}
-          target={"_blank"}
-          rel={"noopener noreferrer"}
+          className="bg-ELSE-EC0 py-2 px-4 text-SYSTEM-white rounded-md"
+          href="https://b2b.ktwiz.co.kr/about/intro"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           kt wiz 스폰서 신청 및 문의
         </Link>

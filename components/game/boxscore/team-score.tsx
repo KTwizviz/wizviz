64b5862ea3414468470b99535d5da57f3a0cf774
재희 <hareppy329@gmail.com>
2025-01-09 14:37:24 +0900

    ♻️ WV-29 rafactor : team logos 상수 분리

--- components/game/boxscore/team-score.tsx
+++ components/game/boxscore/team-score.tsx
@@ -1,29 +1,5 @@
-import Image, { StaticImageData } from "next/image";
-import {
-  logo_HH,
-  logo_HT,
-  logo_KT,
-  logo_LG,
-  logo_LT,
-  logo_NC,
-  logo_OB,
-  logo_SSG,
-  logo_SS,
-  logo_WO,
-} from "@/assets/images/@index";
-
-const teamLogos: Record<Team, StaticImageData> = {
-  한화: logo_HH,
-  KIA: logo_HT,
-  KT: logo_KT,
-  LG: logo_LG,
-  롯데: logo_LT,
-  NC: logo_NC,
-  두산: logo_OB,
-  SSG: logo_SSG,
-  삼성: logo_SS,
-  키움: logo_WO,
-};
+import Image from "next/image";
+import { teamLogos } from "@/constants/team-logos";
 
 const TeamScore = (props: TeamScoreProps) => {
   return (

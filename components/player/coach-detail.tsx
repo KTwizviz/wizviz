4ffa7a4f8d4e0b7279f615b2a81504e3e80ee129
hayoung78 <wendy4332@naver.com>
2025-01-09 13:00:38 +0900

    💄 WV-35 design : coachDetailCard 컴포넌트 생성

--- components/player/coach-detail.tsx
+++ components/player/coach-detail.tsx
@@ -4,18 +4,33 @@ import { player } from "@/assets/images/@index";
 export const CoachDetail = () => {
   return (
     <>
-      <div className="bg-SYSTEM-white border w-full h-[500px]">
+      <div className="bg-SYSTEM-white border w-full h-[455px]">
         <div className="grid grid-cols-2">
           <div className="border">
             <Image src={player} alt="player_image" width={450} height={500} />
           </div>
-          <div className="bg-blue-400 w-full">
+          <div className="w-full p-4 flex flex-col justify-center">
             <div className="text-SYSTEM-main text-xll font-bold">
               NO.1 코칭스텝
             </div>
-            <div className="font-bold text-SYSTEM-black text-xxl">김주일</div>
-            <div className="text-xll">KIM JOO IL</div>
-            <p></p>
+            <div className="font-bold text-SYSTEM-black text-xxl pt-2">
+              김주일
+            </div>
+            <div className="text-xll pt-1 text-ELSE-49 font-light">
+              KIM JOO IL
+            </div>
+            <div className="grid grid-cols-2 gap-1 text-xl pt-5">
+              <p>별명</p>
+              <p>쭈발이</p>
+              <p>키/혈액형</p>
+              <p>190cm/A형</p>
+              <p>취미</p>
+              <p>취미</p>
+              <p>좋아하는선수</p>
+              <p>좋아하는선수</p>
+              <p>좌우명</p>
+              <p>좌우명</p>
+            </div>
           </div>
         </div>
       </div>

201d871f1fe8d801f385ef35ecf762f1eb55c8c7
hayoung78 <wendy4332@naver.com>
2025-01-09 14:20:18 +0900

    ➕ WV-35 feat : pitcher-list api 함수 추가

--- api/player/apis.ts
+++ api/player/apis.ts
@@ -1,10 +1,11 @@
-import { CoachResponse } from "./types";
+import { CoachResponse, PitcherResponse } from "./types";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
 
 // api/player/coachlist
-
 export const getCoachList = async (): Promise<CoachInfo[]> => {
-  const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
-  const res = await fetch(`${BASE_URL}/player/coachlist`, { method: "GET" });
+  const url = `${BASE_URL}/player/coachlist`;
+  const res = await fetch(url, { method: "GET" });
 
   if (!res.ok) {
     throw new Error("Failed to fetch coach list");
@@ -12,4 +13,16 @@ export const getCoachList = async (): Promise<CoachInfo[]> => {
 
   const response: CoachResponse = await res.json();
   return response.data.list; // list 반환
+};
+
+// api/player/pitcherlist
+export const getPitcherList = async (): Promise<PitcherInfo[]> => {
+  const url = `${BASE_URL}/player/pitcherlist`;
+  const res = await fetch(url, { method: "GET" });
+  if (!res.ok) {
+    throw new Error("Failed to fetch coach list");
+  }
+
+  const response: PitcherResponse = await res.json();
+  return response;
 };

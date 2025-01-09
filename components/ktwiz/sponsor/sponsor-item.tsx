25c9276d17ee95626ed81d699a87f1cdbe731889
naelumcat <naelumcat@gmail.com>
2025-01-09 14:00:28 +0900

    💄[WV-39] design : create-sponsor-page map 활용하기

--- components/ktwiz/sponsor/sponsor-item.tsx
+++ components/ktwiz/sponsor/sponsor-item.tsx
@@ -19,136 +19,35 @@ import {
 } from "@/assets/images/@index";
 
 export const SponsorItem = () => {
+  const sponsors = [
+    { src: Sponsor_bccard_img, alt: "Sponsor_bccard" },
+    { src: Sponsor_suwon_img, alt: "Sponsor_suwon" },
+    { src: Sponsor_daishin_img, alt: "Sponsor_daishin" },
+    { src: Sponsor_newbalance_img, alt: "Sponsor_newbalance" },
+    { src: Sponsor_panasonic_img, alt: "Sponsor_panasonic" },
+    { src: Sponsor_usana_img, alt: "Sponsor_usana" },
+    { src: Sponsor_oksavingsbank_img, alt: "Sponsor_oksavingsbank" },
+    { src: Sponsor_ktestate_img, alt: "Sponsor_ktestate" },
+    { src: Sponsor_dongsuh_img, alt: "Sponsor_dongsuh" },
+    { src: Sponsor_nongshim_img, alt: "Sponsor_nongshim" },
+    { src: Sponsor_samsung_img, alt: "Sponsor_samsung" },
+    { src: Sponsor_ticketlink_img, alt: "Sponsor_ticketlink" },
+    { src: Sponsor_tirebank_img, alt: "Sponsor_tirebank" },
+    { src: Sponsor_dorco_img, alt: "Sponsor_dorco" },
+    { src: Sponsor_sangsangin_img, alt: "Sponsor_sangsangin" },
+    { src: Sponsor_onface_img, alt: "Sponsor_onface" },
+  ];
+
   return (
     <div className="grid grid-cols-4 gap-5">
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_bccard_img}
-          alt={"Sponsor_bccard"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_suwon_img}
-          alt={"Sponsor_suwon"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_daishin_img}
-          alt={"Sponsor_daishin"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_newbalance_img}
-          alt={"Sponsor_newbalance"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_panasonic_img}
-          alt={"Sponsor_panasonic"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_usana_img}
-          alt={"Sponsor_usana"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_oksavingsbank_img}
-          alt={"Sponsor_oksavingsbank"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_ktestate_img}
-          alt={"Sponsor_ktestate"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_dongsuh_img}
-          alt={"Sponsor_dongsuh"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_nongshim_img}
-          alt={"Sponsor_nongshim"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_samsung_img}
-          alt={"Sponsor_samsung"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_ticketlink_img}
-          alt={"Sponsor_ticketlink"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_tirebank_img}
-          alt={"Sponsor_tirebank"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_dorco_img}
-          alt={"Sponsor_dorco"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_sangsangin_img}
-          alt={"Sponsor_sangsangin"}
-          width={200}
-          height={100}
-        />
-      </div>
-      <div className="border border-ELSE-7374 flex items-center justify-center p-4">
-        <Image
-          src={Sponsor_onface_img}
-          alt={"Sponsor_onface"}
-          width={200}
-          height={100}
-        />
-      </div>
+      {sponsors.map((sponsor, name) => (
+        <div
+          key={name}
+          className="border border-ELSE-7374 flex items-center justify-center p-4"
+        >
+          <Image src={sponsor.src} alt={sponsor.alt} width={200} height={100} />
+        </div>
+      ))}
     </div>
   );
 };

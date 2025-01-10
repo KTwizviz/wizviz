505a9953e877c1a081261653e1aadac5307510b6
JinwooKim <rlawlsdn9583@gmail.com>
2025-01-10 15:54:02 +0900

    ➕ WV-41 feat : BASE_URL로 엔드포인트 수정

--- api/game/apis.ts
+++ api/game/apis.ts
@@ -39,7 +39,7 @@ export async function getAllSchedules(params: string) {
 export async function getTeamRelativeRecord() {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_KEY}/game/rank/teamvs`,
+      `${BASE_URL}/game/rank/teamvs`,
     );
 
     if (!res.ok) {

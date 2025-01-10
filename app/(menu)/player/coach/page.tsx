3dd7ffa862add3fb07a5f5c99cf40d85e100c469
hayoung78 <wendy4332@naver.com>
2025-01-10 15:42:04 +0900

    🔧 WV-35 fix : build error 수정

--- app/(menu)/player/coach/page.tsx
+++ app/(menu)/player/coach/page.tsx
@@ -1,6 +1,6 @@
 import { getCoachList } from "@/api/player/apis";
 import { CoachList } from "@/components/player/coach-list";
-
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const coachList = await getCoachList();
   return (
--- app/(menu)/player/pitcher/page.tsx
+++ app/(menu)/player/pitcher/page.tsx
@@ -1,6 +1,6 @@
 import { getPitcherList } from "@/api/player/apis";
 import PitcherList from "@/components/player/pitcher-list";
-
+export const dynamic = "force-dynamic";
 export default async function page() {
   const pitcherList = await getPitcherList();
   return (

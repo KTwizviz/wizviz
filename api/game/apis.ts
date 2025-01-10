cc6f2a66892355c0c107bee902174304967cc28c
JinwooKim <rlawlsdn9583@gmail.com>
2025-01-10 14:23:21 +0900

    ➕ WV-41 feat: 팀상대전적 API 함수 추가

--- api/game/apis.ts
+++ api/game/apis.ts
@@ -33,3 +33,21 @@ export async function getAllSchedules(params: string) {
     console.error('API 요청 에러:', error);
   }
 }
+
+export async function getTeamRelativeRecord() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_KEY}/game/rank/teamvs`,
+    );
+
+    if (!res.ok) {
+      throw new Error('네트워크 문제 발생');
+    };
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.error('API 요청 에러:', error);
+  }
+}

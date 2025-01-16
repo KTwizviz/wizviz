49774b74f9e09b7ec6c94bb0b69677cd79cd8f97
재희 <hareppy329@gmail.com>
2025-01-16 15:56:49 +0900

    ♻️ WV-40 refactor : 팀 랭킹 api 엔드포인트 수정

--- api/main/apis.ts
+++ api/main/apis.ts
@@ -1,10 +1,6 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
-
 export const getKtwizRank = async (): Promise<GetKtwizRank> => {
   try {
-    const URL = `${BASE_URL}/game/ktwizteamrank`;
-
-    const response = await fetch(URL);
+    const response = await fetch("/api/game/ktwizteamrank");
 
     if (!response.ok) {
       throw new Error("Network response was not ok");

c46da47b662242a10bfff0d5ba2d52424fa0446d
재희 <hareppy329@gmail.com>
2025-01-16 14:22:58 +0900

    + WV-40 feat : 팀 랭킹 api & type 추가

---
+++ api/main/apis.ts
@@ -0,0 +1,23 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
+
+export const getKtwizRank = async (): Promise<GetKtwizRank> => {
+  try {
+    const URL = `${BASE_URL}/game/ktwizteamrank`;
+
+    const response = await fetch(URL, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const result = await response.json();
+    return result.data.ktWizTeamRank;
+  } catch (error) {
+    console.error("API 요청 에러:", error);
+    throw error;
+  }
+};

884f8ed916de228e69f69d068a86e468b9c24d9b
hayoung78 <wendy4332@naver.com>
2025-01-16 13:51:21 +0900

    ➕ WV-45 feat : news-detail page 생성

--- components/media/news-detail.tsx
+++ components/media/news-detail.tsx
@@ -37,7 +37,7 @@ export const NewsDetail = ({ data }: ArticleContentsProps) => {
   return (
     <div className="max-w-4xl mx-auto p-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold mb-4 bg-SYSTEM-main/30">
+        <h1 className="text-2xl font-bold mb-6 bg-SYSTEM-main/30">
           {data.artcTitle}
         </h1>
         <div className="flex items-center gap-1 h-fit">

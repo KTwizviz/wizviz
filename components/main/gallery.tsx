25e502c9f0e61de939ab16c6a84ea15b7e29cfc7
재희 <hareppy329@gmail.com>
2025-01-16 14:26:52 +0900

    ♻️ WV-40 refactor : 갤러리 하단 스토어 배너, 이달의 선수 배너 클릭 시 페이지 이동

--- components/main/gallery.tsx
+++ components/main/gallery.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import { gallery, store_banner, player_banner } from "@/assets/images/@index";
 import Carousel from "../ui/custom-carousel";
 import { CAROUSE_ITEMS } from "@/constants/main";
+import { useRouter } from "next/navigation";
 
 const Gallery = () => {
+  const router = useRouter();
   return (
     <div className="w-[1200px] justify-self-center relative mb-[100px] justify-items-center">
       <div className="relative h-[620px]">
@@ -22,12 +24,25 @@ const Gallery = () => {
         </div>
       </div>
       <div className="w-[1100px] mt-[40px] flex gap-5 justify-self-center">
-        <Image src={store_banner} alt="Store Banner" width={540} height={190} />
+        <Image
+          src={store_banner}
+          alt="Store Banner"
+          width={540}
+          height={190}
+          className="cursor-pointer"
+          onClick={() => {
+            window.open("https://www.ktwizstore.co.kr", "_blank");
+          }}
+        />
         <Image
           src={player_banner}
           alt="Player of the Month Banner"
           width={540}
           height={190}
+          className="cursor-pointer"
+          onClick={() => {
+            router.push("/player/hitter/infielder/14");
+          }}
         />
       </div>
     </div>

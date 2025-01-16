cc5b3b741c32394096c7c7ace297d13ada5e522c
JinwooKim <rlawlsdn9583@gmail.com>
2025-01-16 15:41:14 +0900

    🔧 WV-46 fix : fix: fetch 엔드포인트 수정

--- api/news/apis.ts
+++ api/news/apis.ts
@@ -1,12 +1,10 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
-
 // news-detail
 export const getNewsDetail = async (
   artcSeq: string
 ): Promise<ArticleContentsResponse["data"]> => {
   const queryparam = artcSeq ? `newsdetail?artcSeq=${artcSeq}` : "";
 
-  const url = `${BASE_URL}/article/${queryparam}`;
+  const url = `/api/article/${queryparam}`;
   const res = await fetch(url);
 
   if (!res.ok) {

74f32700444021600af14208f1801e27c9914589
hayoung78 <wendy4332@naver.com>
2025-01-16 17:36:20 +0900

    ♻️ WV-42 refactor : api 수정

--- api/news/apis.ts
+++ api/news/apis.ts
@@ -1,10 +1,10 @@
 // news-detail
+const BASE_URL = process.env.NEXT_PUBLIC_API_KEY;
 export const getNewsDetail = async (
   artcSeq: string
 ): Promise<ArticleContentsResponse["data"]> => {
   const queryparam = artcSeq ? `newsdetail?artcSeq=${artcSeq}` : "";
-
-  const url = `/api/article/${queryparam}`;
+  const url = `http://${BASE_URL}/api/article/${queryparam}`;
   const res = await fetch(url);
 
   if (!res.ok) {
